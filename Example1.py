6213ef0c25f0265f90c20392dbdca442093f59a0
Jaime Machuca <jaimemachuca@itesm.mx>
2018-01-29 18:13:57 -0600

    Created arm_and_takeoff function, added simple_goto command

--- Example1.py
+++ Example1.py
@@ -2,16 +2,43 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative
 import time
 
 
+def arm_and_takeoff(TargetAltitude):
+	print ("Executing Takeoff")
+
+	while not drone.is_armable:
+		print ("Vehicle is not armable, waiting....")
+		time.sleep(1)
+
+	print("Ready to arm")
+	drone.mode = VehicleMode("GUIDED")
+	drone.armed = True
+
+	while not drone.armed:
+		print("Waiting for arming...")
+		time.sleep(1)
+
+	print("Ready for takeoff, taking off...")
+	drone.simple_takeoff(TargetAltitude)
+
+	while True:
+		Altitude = drone.location.global_relative_frame.alt 
+		print("Altitude: ", Altitude)
+		time.sleep(1)
+
+		if Altitude >= TargetAltitude * 0.95:
+			print("Altitude reached")
+			break
+
+
 
 #Vehicle Connection
 drone = connect('127.0.0.1:14551', wait_ready=True)
+arm_and_takeoff(20)
 
-while not drone.is_armable:
-	print ("Vehicle is not armable, waiting....")
-	time.sleep(1)
+drone.airspeed = 10
 
-print("Ready to arm")
-drone.mode = VehicleMode("GUIDED")
-drone.armed = True
-time.sleep(1)
+a_location = LocationGlobalRelative(-35.362144, 149.164409, 20)
+drone.simple_goto(a_location)
+
+time.sleep(50)
 
